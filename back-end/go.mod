1ca26be293aaa380806e854bdb8a1e6d9015a271
vagrant <vagrant@vagrant.vm>
2023-01-12 13:40:20 +0000

    Back-end first Server

---
+++ back-end/go.mod
@@ -0,0 +1,8 @@
+module github.com/guioliunb/Chain-Services/back-end
+
+go 1.12
+
+require (
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+)
---
+++ back-end/router/router.go
@@ -0,0 +1,12 @@
+package router
+
+import "github.com/gorilla/mux"
+
+type Router struct{
+
+	RawRouter *mux.Router
+}
+
+func (r Router ) GetRawRouter() *mux.Router{
+	return r.RawRouter
+}
