6460c1fb537d7e4eef3f0297c254565fc6012956
vagrant <vagrant@vagrant.vm>
2023-01-12 13:46:34 +0000

    Main initializing the server

--- back-end/main.go
+++ back-end/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/guioliunb/Chain-Services/back-end/server"
+
+
 func main(){
-	
+
+	s := server.NewServer()
+	if err := s.Init(8080); err != nil{
+		panic(err)
+	}
+
+	s.Start()
 }
