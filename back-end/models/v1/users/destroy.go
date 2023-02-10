dbe95b5d7e089ea2118349e04cb3b83fec0a8562
F3866218 <guilherme.loiola@bb.com.br>
2023-02-10 03:42:43 +0000

    Users CRUD v1

--- back-end/models/v1/users/destroy.go
+++ back-end/models/v1/users/destroy.go
@@ -1,1 +1,25 @@
 package users
+
+import (
+	"errors"
+
+	"github.com/guioliunb/Chain-Services/back-end/models"
+)
+
+func Destroy(id string, usr *models.User) (error){
+
+	var exists bool
+
+	for index, user := range mockUsers{
+		if user.ID == id{
+			mockUsers = append(mockUsers[:index], mockUsers[index+1:]... )
+			exists = true
+		}
+	}
+
+	if !exists {
+		return errors.New("Unable to delete user selected") 
+	}
+
+	return nil
+}
--- back-end/models/v1/users/update.go
+++ back-end/models/v1/users/update.go
@@ -1,1 +1,25 @@
 package users
+
+import (
+	"errors"
+
+	"github.com/guioliunb/Chain-Services/back-end/models"
+)
+
+func Update(id string, usr *models.User) (*models.User, error){
+
+	var exists bool
+
+	for index, user := range mockUsers{
+		if user.ID == id{
+			mockUsers[index] = *usr
+			exists = true
+		}
+	}
+
+	if !exists {
+		return nil, errors.New("Unable to update user selected") 
+	}
+
+	return usr, nil
+}
