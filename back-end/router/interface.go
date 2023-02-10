ba7ded1e1074bbd16a16ba5f753f719708847b2d
F3866218 <guilherme.loiola@bb.com.br>
2023-02-10 02:35:51 +0000

    Updating models

--- back-end/router/interface.go
+++ back-end/router/interface.go
@@ -1,14 +1,34 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Service interface{
 	GetRawRouter() *mux.Router
 }
 
+const (
+	staticDir = "/static/"
+)
+
 func GetRouter() Service{
 	r := Router{
 		RawRouter: mux.NewRouter().StrictSlash(true),
+	}
+
+	r.RawRouter.
+		PathPrefix(staticDir).
+		Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir))))
+
+	for _, route := range GetRoutes() {
+		r.RawRouter.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
 	}
 
 	return r
