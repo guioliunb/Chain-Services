f1357389ad2d210b14401a5599a1889b5c73fb33
Guilherme Oliveira Loiola <52641472+guioliunb@users.noreply.github.com>
2023-02-17 01:30:06 -0300

    Fixing create button bug

--- front-end/src/views/Create.vue
+++ front-end/src/views/Create.vue
@@ -1,68 +1,63 @@
-<template lang = "pug">
-v-row
-    v-col(md="6" offset-md= "2")
-        v-text-field(
-            label = "Name"
-            v-model = "name"
-        )
-        v-text-field(
-            label = "Score"
-            v-model = "score"
-        )
-        v-text-field(
-            label = "Source"
-            v-model = "source"
-        )
+<template lang="pug">
+    
 
+    v-row
 
-        v-select(
-            label = "Type"
-            v-model = "type_id"
-            :items = "raw_resources"
-            item-text = "role"
-            item-value = "id"
-        )
+        v-col(md="6" offset-md="2")
 
-        v-btn(color = "sucess" @click= "create" ) Create
+            v-text-field(
+                label="Name"
+                v-model="name"
+            )
+
+            v-text-field(
+                label="Weight"
+                v-model="weight"
+            )
+
+            v-select(
+                label="Type"
+                v-model="type_id"
+                :items="raw_resource_types"
+                item-text="name"
+                item-value="id"
+            )
+
+            v-btn(color="success" @click="create") Create
 
 </template>
-  
-<script>
 
-import { mapState } from 'vuex';
+<script>
+import { mapState } from "vuex";
 
 export default {
-    name: "create",
-    data(){
+  name: "create",
+  data() {
     return {
-        name : "",
-        score: 0,
-        source : "",    
-        type_id: 1
+      name: "",
+      weight: 42000,
+      type_id: 0
     };
-    },
-    computed:{
-      ...mapState(
-        {raw_resources: state => state.raw_resource_types}
-      ),
-    },
-    methods: {
-        async create(){
-            const vm = this;
+  },
+  computed: {
+    ...mapState({
+      raw_resource_types: state => state.raw_resource_types
+    })
+  },
+  methods: {
+    async create() {
+      const vm = this;
 
-            await vm.$store.dispatch("create", {
-                name: vm.name,
-                source : vm.source,
-                score: vm.score,
-                type_id: vm.type_id
+      await vm.$store.dispatch("create", {
+        name: vm.name,
+        weight: vm.weight,
+        type_id: vm.type_id
+      });
 
-            });
+      await vm.$store.dispatch("fetchRawResource");
 
-            vm.$router.push({
-                path: "/"
-            });
-
-        }
+      vm.$router.push({ path: "/" });
     }
-}
+  }
+};
 </script>
