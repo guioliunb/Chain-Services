c6f03132df735d693ea3fc3d7b70feb86a8301d7
Guilherme Oliveira Loiola <guilhermeoliveira.0210@gmail.com>
2023-05-03 13:32:02 -0300

    Trying to use smart contracts

--- back-end/models/rawresources.go
+++ back-end/models/rawresources.go
@@ -4,16 +4,15 @@ import "time"
 
 type RawResourceTypes []RawResourceType
 
-
-type RawResourceType struct{
-	ID string `json: "id"`
+type RawResourceType struct {
+	ID   string `json: "id"`
 	Name string `json: "name"`
 }
 
-func NewRawResourceType(name string) (rawResourceType *RawResourceType, err error){
-	rawResourceType = new (RawResourceType)
+func NewRawResourceType(name string) (rawResourceType *RawResourceType, err error) {
+	rawResourceType = new(RawResourceType)
 
-	if rawResourceType.ID, err = genUUID(); err != nil{
+	if rawResourceType.ID, err = genUUID(); err != nil {
 		return
 	}
 
@@ -22,38 +21,54 @@ func NewRawResourceType(name string) (rawResourceType *RawResourceType, err erro
 	return
 }
 
-
 type RawResources []RawResource
 
-
-type RawResource struct{
-	ID string `json: "id"`
-	Name string `json: "name"`
-	TypeID string `json: "type_id"`
-	Weight int `json: "weight"`
+type RawResource struct {
+	ID          string     `json: "id"`
+	Name        string     `json: "name"`
+	TypeID      string     `json: "type_id"`
+	Weight      int        `json: "weight"`
 	ArrivalTime *time.Time `json: "arrival_time"`
-	Timestamp *time.Time `json: "timestamp"`
+	Timestamp   *time.Time `json: "timestamp"`
 }
 
-func NewRawResource(name string, typeId string, weight int, arrivalTime *time.Time) (rawResource *RawResource, err error){
-	rawResource = new (RawResource)
+func NewRawResource(name string, typeId string, weight int, arrivalTime *time.Time) (rawResource *RawResource, err error) {
+	rawResource = new(RawResource)
 
-	if rawResource.ID, err = genUUID(); err != nil{
+	if rawResource.ID, err = genUUID(); err != nil {
 		return
 	}
 
 	rawResource.Name = name
-	rawResource.TypeID = typeId 
+	rawResource.TypeID = typeId
 	rawResource.Weight = weight
-	
+
 	t := time.Now()
-	if arrivalTime == nil{
+	if arrivalTime == nil {
 		rawResource.ArrivalTime = arrivalTime
 	} else {
 		rawResource.ArrivalTime = &t
 	}
 
+	rawResource.Timestamp = &t
 
+	return
+}
+
+func NewRawResourceDuo(name string, typeId string, weight int) (rawResource *RawResource, err error) {
+	rawResource = new(RawResource)
+
+	if rawResource.ID, err = genUUID(); err != nil {
+		return
+	}
+
+	rawResource.Name = name
+	rawResource.TypeID = typeId
+	rawResource.Weight = weight
+
+	t := time.Now()
+
+	rawResource.ArrivalTime = &t
 	rawResource.Timestamp = &t
 
 	return
--- back-end/models/v1/rawresources/store.go
+++ back-end/models/v1/rawresources/store.go
@@ -1,20 +1,95 @@
 package rawresources
 
 import (
-	"time"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"strconv"
 
 	"github.com/guioliunb/Chain-Services/back-end/models"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-func Store(name string, typeId string, weight int, arrivalTime *time.Time)(rawresource *models.RawResource, err error){
+type SmartContract struct {
+	contractapi.Contract
+}
 
-	rawresource, err = models.NewRawResource(name, typeId, weight, arrivalTime)
+type TransactionContextInterface struct {
+	contractapi.TransactionContextInterface
+}
 
-	if err != nil{
+func (s *SmartContract) Store(ctx contractapi.TransactionContextInterface, name string, typeId string, weight int) (rawresource *models.RawResource, err error) {
+
+	rawresource, err = models.NewRawResourceDuo(name, typeId, weight)
+
+	if err != nil {
 		return
 	}
 
 	mockRawResources = append(mockRawResources, *rawresource)
 
+	/*Blockchain store*/
+
+	/*exists, err := s.AssetExists(ctx, id)
+
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the asset %s already exists", id)
+	}
+	*/
+
+	asset := models.Asset{
+		ID:             "UNIQUE",
+		Color:          "color",
+		Size:           15,
+		Owner:          "owner",
+		AppraisedValue: 15,
+	}
+
+	assetJSON, err := json.Marshal(asset)
+	id := "id#" + strconv.Itoa(rand.Intn(1000))
+
+	ctx.GetStub().PutState(id, assetJSON)
+
+	assetJSONOUT, err := ctx.GetStub().GetState(id)
+	fmt.Printf("mystr:\t %v \n", []byte(assetJSONOUT))
+	/*var assetOUT models.Asset
+	err = json.Unmarshal(assetJSONOUT, &assetOUT)*/
+
 	return
+}
+
+func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return assetJSON != nil, nil
+}
+
+func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
+	exists, err := s.AssetExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the asset %s already exists", id)
+	}
+
+	asset := models.Asset{
+		ID:             id,
+		Color:          color,
+		Size:           size,
+		Owner:          owner,
+		AppraisedValue: appraisedValue,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
 }
